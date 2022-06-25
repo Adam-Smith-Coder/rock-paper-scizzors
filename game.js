dda68e457099dfadc77a4c9acbcc3c6896ded0c8
Adam-Smith-Coder <adcollects@gmail.com>
2022-06-25 10:15:15 +0100

    Update game.js with variables to keep score and play game
    Create variables for player score and computer score
    When game is win add player score ++ and when game is lose add computer
    score ++
    
    create function called game that has a for loop to play the play round
    function 5 times
    Add the user input and computer input to this function so they are
    called upon every round
    Make the user input a prompt that asks for Rock, Paper or Scizzors
    console.log the play round function within the game function to ensure
    output of this is returned

--- game.js
+++ game.js
@@ -5,6 +5,12 @@ function computerPlay () {
     let rand = choice[Math.floor(Math.random()*choice.length)];
     return rand;
 }
+
+let playerSelection
+let computerSelection
+let playerScore = 0
+let computerScore = 0
+
 //This is the code that compares player choice vs computer choice and declares a round winner//
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
@@ -12,14 +18,22 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "It's a Tie"
     } else if ((playerSelection === 'rock' && computerSelection === 'scizzors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scizzors' && computerSelection === 'paper')) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`
+        playerScore++;
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
     } else if ((computerSelection === 'rock' && playerSelection === 'scizzors') || (computerSelection === 'paper' && playerSelection === 'rock') || (computerSelection === 'scizzors' && playerSelection === 'paper')) {
+        computerScore++;
         return `You Lose! ${computerSelection} beats ${playerSelection}`
     } else {
-        return "This is not a choice"
+        return "This is not a valid choice"
     }
 }
 
-const playerSelection = 'Paper';
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection)) 
+function game() {
+    for (let i = 0; i < 5; i++){
+        playerSelection = prompt("Choose Rock, Paper or Scizzors");
+        computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+console.log(game())
