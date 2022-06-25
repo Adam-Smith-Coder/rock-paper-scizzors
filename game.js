25d4eb2a8b14415a1031a1ac0e3a6176610ddae3
Adam-Smith-Coder <adcollects@gmail.com>
2022-06-25 10:58:28 +0100

    Update game.js with code to declare outcome of game function
    Update game function with if/else statement to show the winner of the game
    by showing the player and computer scores as well as declaring a message
    that tells you if you win/lose/draw the game overall
    Add player selection variable to the 'tied round' message so that it
    doesnt stack in console.log if a round is tied twice in a row
    Add comments within game.js to explain key areas of the code and what they
    do

--- game.js
+++ game.js
@@ -13,27 +13,42 @@ let computerScore = 0
 
 //This is the code that compares player choice vs computer choice and declares a round winner//
 function playRound(playerSelection, computerSelection) {
+    //this code ensures that user input is case insensitive//
     playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection === computerSelection) {
-        return "It's a Tie"
+        return `It's a Tied round, choice was ${playerSelection}`
     } else if ((playerSelection === 'rock' && computerSelection === 'scizzors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scizzors' && computerSelection === 'paper')) {
         playerScore++;
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        return `You Win the round! ${playerSelection} beats ${computerSelection}`;
     } else if ((computerSelection === 'rock' && playerSelection === 'scizzors') || (computerSelection === 'paper' && playerSelection === 'rock') || (computerSelection === 'scizzors' && playerSelection === 'paper')) {
         computerScore++;
-        return `You Lose! ${computerSelection} beats ${playerSelection}`
+        return `You Lose the round! ${computerSelection} beats ${playerSelection}`
     } else {
+        //needed to ensure that user is aware their choice was not correct//
         return "This is not a valid choice"
     }
 }
 
 function game() {
     for (let i = 0; i < 5; i++){
+        //these variables are needed here so they are declared for every round//
         playerSelection = prompt("Choose Rock, Paper or Scizzors");
         computerSelection = computerPlay();
         console.log(playRound(playerSelection, computerSelection));
     }
+    //this part of the code logs the final score and declares the outcome of the game//
+    if (i = 5 && computerScore > playerScore) {
+        console.log(`Your Score ${playerScore} Computer Score ${computerScore}`)
+        return "Game Over, you have lost"
+    } else if (i = 5 && playerScore > computerScore) {
+        console.log(`Your Score ${playerScore} Computer Score ${computerScore}`)
+        return "Victory, you have beat the computer"
+    } else if (i = 5 && playerScore === computerScore) {
+        console.log(`Your Score ${playerScore} Computer Score ${computerScore}`)
+        return "The game has ended in a stalemate"
+    }
 }
 
+//required to actually run the game function//
 console.log(game())
