850c1b4069fc9a5994746438a1baa762bf429dbb
Adam-Smith-Coder <adcollects@gmail.com>
2022-06-25 08:47:33 +0100

    Clean up playRound code and add some descriptive comments
    Add comments for the computer selection function and the play round
    function to identify them and what they do
    
    Clean up playRound code by making the choice of the player case insensitive
    which can be achieved by forcing the player input to become lower case
    Update the computer choice array to also be lower case so these match
    Update the If/Else statement to be clear for the new lower case strings
    as well as being clear for a loss condition
    When the loss condition is clear add an else that says any other input
    is not a valid choice

--- game.js
+++ game.js
@@ -1,20 +1,25 @@
-const choice = ['Rock', 'Paper', 'Scizzors'];
+//This code controls the random choice of the computer opponent//
+const choice = ['rock', 'paper', 'scizzors'];
 
 function computerPlay () {
     let rand = choice[Math.floor(Math.random()*choice.length)];
     return rand;
 }
-
+//This is the code that compares player choice vs computer choice and declares a round winner//
 function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
     if (playerSelection === computerSelection) {
         return "It's a Tie"
-    } else if ((playerSelection === 'Rock' && computerSelection === 'Scizzors') || (playerSelection === 'Paper' && computerSelection === 'Rock') || (playerSelection === 'Scizzors' && computerSelection === 'Paper')) {
+    } else if ((playerSelection === 'rock' && computerSelection === 'scizzors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scizzors' && computerSelection === 'paper')) {
         return `You Win! ${playerSelection} beats ${computerSelection}`
-    } else {
+    } else if ((computerSelection === 'rock' && playerSelection === 'scizzors') || (computerSelection === 'paper' && playerSelection === 'rock') || (computerSelection === 'scizzors' && playerSelection === 'paper')) {
         return `You Lose! ${computerSelection} beats ${playerSelection}`
+    } else {
+        return "This is not a choice"
     }
 }
 
-const playerSelection = 'Rock';
+const playerSelection = 'Paper';
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection)) 
